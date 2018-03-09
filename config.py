9c12d6cfb2007e48221975963e06bc83aaa8dfc2
Brooke Kennedy <brookekennedy2018@u.northwestern.edu>
2018-03-09 01:15:28 -0500

    updated gitignore

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://animaloutcomes:Desktop01@animaloutcomesdatabase.c76qrkgxophn.us-east-1.rds.amazonaws.com/animaldb'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://animaloutcomes:Desktop01@animaloutcomesdatabase.c76qrkgxophn.us-east-1.rds.amazonaws.com/animaldb'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False 
 	
 	
